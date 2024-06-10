bdbed57116c9970ec24233c1aa1cece35ec85deb
Mohit Suman <mohit.suman.tech@gmail.com>
2024-06-10 11:32:42 +0000

    initial commit to check for tailwind CSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <p className='text-2xl'>Hello! Myself <span className='italic'>Mohit Suman</span>, I am Coder & Photographer</p>
     </div>
   );
 }
